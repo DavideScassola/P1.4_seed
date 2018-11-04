f219c5e5c1f856e5b4f60307cb60de2c691921cd
Davide Scassola <projanko@gmail.com>
2018-11-04 22:34:06 +0100

    Hi

---
+++ notes/updater.sh
@@ -0,0 +1,9 @@
+cd /home/davide/1✪1✪3/DSSC/Corsi/AdvanceProgramming/advanced_programming-2018-19
+	./autoUpdate.sh 
+
+cd /home/davide/1✪1✪3/DSSC/Corsi/HPC/P1.2_seed
+	./autoUpdate.sh
+
+cd /home/davide/1✪1✪3/DSSC/Corsi/NumericalAnalysis/P1.4_seed
+	./autoUpdate.sh
+
